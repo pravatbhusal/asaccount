c6bc2bb888aa037248b531a521105ed162cc94e3
Pravat Bhusal <mist.bhusal@gmail.com>
2016-07-08 17:16:52 -0500

    Create SQL.sql

---
+++ SQL/SQL.sql
@@ -0,0 +1,20 @@
+-- phpMyAdmin SQL Dump
+-- version 2.11.4
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 08, 2016 at 06:16 PM
+-- Server version: 5.1.57
+-- PHP Version: 5.2.17
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `a6934122_lumosit`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
