1f187e459406b5a875b8df32f3b02d7a51f38820
Pravat Bhusal <mist.bhusal@gmail.com>
2016-07-08 17:10:49 -0500

    Create ConnectSQL.php

---
+++ PHP/ConnectSQL.php
@@ -0,0 +1,29 @@
+<?php
+ 
+//Database Connection Variables
+
+$db_name = ";
+ 
+$db_username = "";
+ 
+$db_password = "";
+ 
+$db_host = "";
+ 
+/*
+mysql_connect is a built in function that allows us to make an easy connection.
+*/
+ 
+mysql_connect($db_host, $db_username, $db_password, $db_name);
+ 
+/*
+mysql_select_db is a built in function that allows us to select the database. This is an essential function.
+ 
+We use the 'die' function to check for errors.
+ 
+*/
+ 
+mysql_select_db($db_name) or die (mysql_error());
+ 
+ 
+?>
