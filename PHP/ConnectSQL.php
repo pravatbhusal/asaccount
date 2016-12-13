df794d6b16f5f9164666163bdf15d7ca9ad78015
Pravat Bhusal <mist.bhusal@gmail.com>
2016-12-12 19:47:45 -0600

    Add String templates for SQL variables.

--- PHP/ConnectSQL.php
+++ PHP/ConnectSQL.php
@@ -2,13 +2,13 @@
  
 //Database Connection Variables
 
-$db_name = ";
+$db_name = "DATABASE_NAME";
  
-$db_username = "";
+$db_username = "DATABASE_USERNAME";
  
-$db_password = "";
+$db_password = "DATABASE_PASSWORD";
  
-$db_host = "";
+$db_host = "DATABASE_HOST";
  
 /*
 mysql_connect is a built in function that allows us to make an easy connection.
